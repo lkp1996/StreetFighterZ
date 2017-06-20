26930ad24e6a4d04e12669e3975a73bb53926978
doudiny <yann.doudin@gmail.com>
2017-06-20 16:01:02 +0200

    switch scene ok
    switch scene (back et one player) fonctionne

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,26 +1,25 @@
 package application;
-	
+
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-
-public class Main extends Application {
-	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+public class Main extends Application{
 	
+	
+
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		VBox root = FXMLLoader.load(getClass().getResource("StreetFighterZMain.fxml"));
+		Scene scene = new Scene(root);
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+
 }
--- src/application/MainView.java
+++ src/application/MainView.java
@@ -1,33 +1,9 @@
 package application;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
-public class MainView extends Application{
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-	try {
-	    MainController ctrl;
-
-	    FXMLLoader loader = new FXMLLoader();
-	    loader.setLocation(getClass().getResource("StreetFighterZMain.fxml"));
-	    GridPane root = (GridPane) loader.load();
-
-	    ctrl = (MainController) loader.getController();
-	    ctrl.init();
-
-	    Scene scene = new Scene(root);
-	    primaryStage.setScene(scene);
-	    primaryStage.setTitle("StreetFighterZ");
-	    primaryStage.show();
-	} catch (Exception e) {
-	    e.printStackTrace();
-	}
+public class MainView {
 	
-    }
+	
 
 }
