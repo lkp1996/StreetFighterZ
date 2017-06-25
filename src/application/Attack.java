858ef61ddd11d006eb70a8bd93db694a9eeb000f
doudiny <yann.doudin@gmail.com>
2017-06-25 14:36:44 +0200

    Ajout du coup critique
    par contre sans indications

--- src/application/Attack.java
+++ src/application/Attack.java
@@ -1,48 +1,55 @@
 package application;
 
+import java.util.Random;
+
 public class Attack {
-    private String name;
-    private int damage;
-    private double criticalChance;
-    private double timesCriticalDamage;
+	private String name;
+	private int damage;
+	private double criticalChance;
+	private double timesCriticalDamage;
 
-    public Attack(String name, int damage, double criticalChance, double timesCriticalDamage) {
-	this.name = name;
-	this.damage = damage;
-	this.criticalChance = criticalChance;
-	this.timesCriticalDamage = timesCriticalDamage;
-    }
+	public Attack(String name, int damage, double criticalChance, double timesCriticalDamage) {
+		this.name = name;
+		this.damage = damage;
+		this.criticalChance = criticalChance;
+		this.timesCriticalDamage = timesCriticalDamage;
+	}
 
-    public String getName() {
-	return name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public void setName(String name) {
-	this.name = name;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public int getDamage() {
-	return damage;
-    }
+	public int getDamage() {
+		int dmgtmp = damage;
+		Random r = new Random();
+		int rndm = r.nextInt(99) + 1;
+		if (rndm < criticalChance)
+			dmgtmp = (int) (dmgtmp * timesCriticalDamage);
+		return dmgtmp;
+	}
 
-    public void setDamage(int damage) {
-	this.damage = damage;
-    }
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
 
-    public double getCriticalChance() {
-	return criticalChance;
-    }
+	public double getCriticalChance() {
+		return criticalChance;
+	}
 
-    public void setCriticalChance(double criticalChance) {
-	this.criticalChance = criticalChance;
-    }
+	public void setCriticalChance(double criticalChance) {
+		this.criticalChance = criticalChance;
+	}
 
-    public double getTimesCriticalDamage() {
-	return timesCriticalDamage;
-    }
+	public double getTimesCriticalDamage() {
+		return timesCriticalDamage;
+	}
 
-    public void setTimesCriticalDamage(double timesCriticalDamage) {
-	this.timesCriticalDamage = timesCriticalDamage;
-    }
+	public void setTimesCriticalDamage(double timesCriticalDamage) {
+		this.timesCriticalDamage = timesCriticalDamage;
+	}
 
 }
